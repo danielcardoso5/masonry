8743d8bbf5974b70867bd84074b4246aa9ee13a9
danielcardoso5 <d.r.cardoso5@gmail.com>
2016-05-25 15:05:56 +0100

    Changed _getItemLayoutPosition function to sort items from left to right

--- masonry.js
+++ masonry.js
@@ -99,17 +99,30 @@
     // round if off by 1 pixel, otherwise use ceil
     var colSpan = Math[ mathMethod ]( item.size.outerWidth / this.columnWidth );
     colSpan = Math.min( colSpan, this.cols );
-
+  
     var colGroup = this._getColGroup( colSpan );
+    // ### HACK: sort by natural order, not by min col height
     // get the minimum Y value from the columns
-    var minimumY = Math.min.apply( Math, colGroup );
-    var shortColIndex = colGroup.indexOf( minimumY );
-
+    // var minimumY = Math.min.apply( Math, colGroup );
+    // var shortColIndex = utils.indexOf( colGroup, minimumY );
+    var shortColIndex = jQuery(item.element).index() % colGroup.length;
+    var minimumY = colGroup[shortColIndex];
+  
     // position the brick
     var position = {
       x: this.columnWidth * shortColIndex,
       y: minimumY
     };
+  
+    // apply setHeight to necessary columns
+    var setHeight = minimumY + item.size.outerHeight;
+    var setSpan = this.cols + 1 - colGroup.length;
+    for ( var i = 0; i < setSpan; i++ ) {
+      this.colYs[ shortColIndex + i ] = setHeight;
+    }
+  
+    return position;
+  };
 
     // apply setHeight to necessary columns
     var setHeight = minimumY + item.size.outerHeight;
